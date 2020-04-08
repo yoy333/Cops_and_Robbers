f3202ec51638c3903cf67c59294810761cb67628
yoy333 <colinjdoherty@outlook.com>
2020-04-08 12:24:57 -0700

    imgs

--- Classes/Gem.js
+++ Classes/Gem.js
@@ -2,6 +2,11 @@ var Gem = function(){
 	this.collision = false
 	this.content = 'gem'
 	this.color = "#FF0000"
+	
+	this.draw = function(x, y, px, py){
+		var img = document.getElementById("gemImg");
+		ctx.drawImage(img, x-px, y-py);
+	}
 }
 
 Gem.prototype = new Particle();

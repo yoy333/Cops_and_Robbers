ba7c308c73d91e5d12b5968b9cfe35a04cc97831
yoy333 <colinjdoherty@outlook.com>
2020-04-09 18:47:23 -0700

    furniture glitch

--- Classes/furniture.js
+++ Classes/furniture.js
@@ -2,9 +2,24 @@ var Furniture = function(){
 	this.collision = true
 	this.content='furniture'
 	this.color = "#B5651D"
-  	
+	this.activePress = false
+	
+	this.newPress = function(){
+		if(keys.e){
+			if(!this.activePress){
+				this.activePress = true
+				return true
+			}else{
+				return false
+			}
+		}else{
+			this.activePress = false
+			return false
+		}
+	}
+ 	
 	this.hide = function(){
-		if(playerA[0].x_pos>=this.x_pos-50&&playerA[0].x_pos<=this.x_pos+50&&playerA[0].y_pos>=this.y_pos-50&&playerA[0].y_pos<=this.y_pos+50&&playerA[0].hiding==false&&keys.e){
+		if(playerA[0].x_pos>=this.x_pos-50&&playerA[0].x_pos<=this.x_pos+50&&playerA[0].y_pos>=this.y_pos-50&&playerA[0].y_pos<=this.y_pos+50&&playerA[0].hiding==false&&this.newPress()){
 			playerA[0].x_pos=this.x_pos
 			playerA[0].y_pos=this.y_pos
 			playerA[0].hiding = true
@@ -12,7 +27,7 @@ var Furniture = function(){
 	}
 	
 	this.unhide = function(){
-		if(playerA[0].hiding&&keys.e){
+		if(playerA[0].hiding&&this.newPress()){
 			this.collision = false
 			playerA[0].hiding = false
 		}
